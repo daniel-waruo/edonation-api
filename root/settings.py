ad3f1201ae7adabd04e2513a60990c3c35de7373
Daniel Waruo <waruodaniel@gmail.com>
2021-01-21 10:56:43 +0300

    updated default from email

--- root/settings.py
+++ root/settings.py
@@ -195,7 +195,6 @@ EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'noreply@edonation.co.ke'
 
 # GRAPHENE CONFIGURATION
 GRAPHENE = {
